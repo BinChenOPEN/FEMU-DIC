c47b49374f5c2f066c8f3326e0963c8756306240
Bin Chen <cbbuaa@outlook.com>
2025-02-18 10:16:11 +0100

    Add files via upload

--- 2D-DIC/post_plot_results.m
+++ 2D-DIC/post_plot_results.m
@@ -1,10 +1,12 @@
 clear, clc,
-% load the data to plot. 
+%% Specify the parameters in the following
+Params_plot = 'exy'; % select between exx, eyy,exy, u, v
 Params_file = '/Users/binchen/Downloads/FEMU-DIC-main-4/Demo/VirtualImage/Params.mat'; % parameter files
 data_file = '/Users/binchen/Downloads/FEMU-DIC-main-4/Demo/VirtualImage/Img_Simu_0001.mat'; % data file for the figure
 ImRef_file = '/Users/binchen/Downloads/FEMU-DIC-main-4/Demo/VirtualImage/Img_Simu_0000.bmp'; % Reference image
 ImDef_file = '/Users/binchen/Downloads/FEMU-DIC-main-4/Demo/VirtualImage/Img_Simu_0001.bmp'; % Deformed image
 
+%%
 load(Params_file);
 load(data_file);
 
@@ -20,4 +22,4 @@ subplot(1,2,1);
 imagesc(ImRef);
 axis tight,axis equal,axis off
 
-plotOnImg(Params, ImDef,comptPoints, Disp,Strain, 'exy'); % select between exx, eyy,exy, u, v
+plotOnImg(Params, ImDef,comptPoints, Disp,Strain, Params_plot); 
