fd9e61682ee5121d74adcb88c99183cf1ff3157c
Bin Chen <cbbuaa@outlook.com>
2025-02-18 10:08:04 +0100

    Add files via upload
    Add a file to plot the strain fields and displacement fields

---
+++ 2D-DIC/post_plot_results.m
@@ -0,0 +1,23 @@
+clear, clc,
+% load the data to plot. 
+Params_file = '/Users/binchen/Downloads/FEMU-DIC-main-4/Demo/VirtualImage/Params.mat'; % parameter files
+data_file = '/Users/binchen/Downloads/FEMU-DIC-main-4/Demo/VirtualImage/Img_Simu_0001.mat'; % data file for the figure
+ImRef_file = '/Users/binchen/Downloads/FEMU-DIC-main-4/Demo/VirtualImage/Img_Simu_0000.bmp'; % Reference image
+ImDef_file = '/Users/binchen/Downloads/FEMU-DIC-main-4/Demo/VirtualImage/Img_Simu_0001.bmp'; % Deformed image
+
+load(Params_file);
+load(data_file);
+
+ImRef = imread(ImRef_file); 
+ImDef = imread(ImDef_file);
+
+if size(ImRef,3)>1
+    ImRef = rgb2gray(ImDef);
+    ImDef = rgb2gray(ImDef);    
+end
+ImRef = repmat(uint8(ImRef),1,1,3);
+subplot(1,2,1);
+imagesc(ImRef);
+axis tight,axis equal,axis off
+
+plotOnImg(Params, ImDef,comptPoints, Disp,Strain, 'exy'); % select between exx, eyy,exy, u, v
